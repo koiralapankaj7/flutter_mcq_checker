d2400edb55c78d6f52360ef970f90ef12e4876ad
cricpunk <koiralapankaj007@gmail.com>
2019-04-26 08:51:21 +0545

    validation mixin updated

--- lib/src/blocs/validation_mixin.dart
+++ lib/src/blocs/validation_mixin.dart
@@ -1,13 +1,64 @@
 import 'dart:async';
 
 class ValidationMixin {
+  // Validation for module
   final StreamTransformer validateModule =
       StreamTransformer<String, String>.fromHandlers(
-          handleData: (String module, EventSink<String> sink) {
-    if (module.length > 4) {
-      sink.add(module);
-    } else {
-      sink.addError("Please enter valid module name");
-    }
-  });
+    handleData: (String module, EventSink<String> sink) {
+      if (module.length > 4) {
+        sink.add(module);
+      } else {
+        sink.addError("Please enter valid module name");
+      }
+    },
+  );
+
+  // Validation for year
+  final StreamTransformer validateYear =
+      StreamTransformer<String, String>.fromHandlers(
+    handleData: (String year, EventSink<String> sink) {
+      if ((year.length == 4) && (num.tryParse(year) != null)) {
+        sink.add(year);
+      } else {
+        sink.addError('Please enter valid year.');
+      }
+    },
+  );
+
+  // Validation for sem
+  StreamTransformer validateSem =
+      StreamTransformer<String, String>.fromHandlers(
+    handleData: (String sem, EventSink<String> sink) {
+      if ((sem.length == 1) && (int.tryParse(sem) != null)) {
+        sink.add(sem);
+      } else {
+        sink.addError('Semester value must be like 1, 2, 3 ...');
+      }
+    },
+  );
+
+  // Validation for group
+  StreamTransformer validateGroup =
+      StreamTransformer<String, String>.fromHandlers(
+    handleData: (String sem, EventSink<String> sink) {
+      if (sem.length > 0) {
+        sink.add(sem);
+      } else {
+        sink.addError('Group should not be empty.');
+      }
+    },
+  );
+
+  // Validation for answer
+  StreamTransformer validateAnswer =
+      StreamTransformer<Map<int, String>, Map<int, String>>.fromHandlers(
+    handleData: (Map<int, String> answer, EventSink<Map<int, String>> sink) {
+      if (answer.isNotEmpty) {
+        sink.add(answer);
+      } else {
+        sink.addError(
+            'Sorry ! could not scan the image. Try different image or enter answer manually');
+      }
+    },
+  );
 }
